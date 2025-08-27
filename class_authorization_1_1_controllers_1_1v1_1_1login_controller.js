29c0ece8ad3eb33336fb98204e7803ab7d5a9e6b
elsairaKrom <elsa.caicero@kromlogistica.com>
2025-08-27 17:28:09 -0600

    Cambios

--- class_authorization_1_1_controllers_1_1v1_1_1login_controller.js
+++ class_authorization_1_1_controllers_1_1v1_1_1login_controller.js
@@ -1,4 +1,5 @@
 var class_authorization_1_1_controllers_1_1v1_1_1login_controller =
 [
+    [ "loginController", "class_authorization_1_1_controllers_1_1v1_1_1login_controller.html#a9ff94ed4e6cb60f8f0c6e9210166c829", null ],
     [ "login", "class_authorization_1_1_controllers_1_1v1_1_1login_controller.html#ace9c6e24a554cc4e8aceae6723ddcff4", null ]
 ];
--- search/all_4.js
+++ search/all_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['login_0',['login',['../class_authorization_1_1_controllers_1_1v1_1_1login_controller.html#ace9c6e24a554cc4e8aceae6723ddcff4',1,'Authorization::Controllers::v1::loginController']]],
-  ['logincontroller_1',['loginController',['../class_authorization_1_1_controllers_1_1v1_1_1login_controller.html',1,'Authorization::Controllers::v1']]]
+  ['logincontroller_1',['loginController',['../class_authorization_1_1_controllers_1_1v1_1_1login_controller.html',1,'Authorization.Controllers.v1.loginController'],['../class_authorization_1_1_controllers_1_1v1_1_1login_controller.html#a9ff94ed4e6cb60f8f0c6e9210166c829',1,'Authorization.Controllers.v1.loginController.loginController()']]]
 ];
